bd7c41637aa3783593941dada2704e8a91f38a7d
Victor Pereira <vpereira@web.de>
2010-05-06 11:16:29 +0200

    added example page

--- jflash.js
+++ jflash.js
@@ -1,22 +1,27 @@
 //Params:
 //message -> the message that should be passed
+//fadeIN -> ms for fadein effect
+//FadeOut -> ms for fadeout effect
+//timeout -> time that we will show our flash message
 //flash -> the flash that you want (notice, error)
 //author Victor Pereira <victor.pereira@bigrails.com>
 (function (jQuery) {
 	$.fn.jflash = function(params){
 	    var flash_div = jQuery(this);
-		params = $.extend( {message: "Action successfully executed", flash: "notice"}, params); 
+		params = $.extend( {message: "Action successfully executed", flash: "notice", fadein:600, fadout:600, timeout:1400}, params); 
 	    
 		var div_name = "flash_" + params.flash
 		flash_div.append("<div id="+ div_name + ">" + params.message  + "</div>");
 		flash_div.show();
-	    flash_div.fadeIn(600);
+	    flash_div.fadeIn(params.fadein);
 	    // use Javascript timeout function to delay calling
 	
 	    setTimeout(function(){
-			flash_div.fadeOut(500,function(){
+			flash_div.fadeOut(params.fadeout,function(){
 				flash_div.html("");
-	    		flash_div.hide()})},1400);
+	    		flash_div.hide()
+			})
+		},params.timeout);
 		return this;
 	};
 })(jQuery);
