c44f6010a2e89045e9163deaec36517a03df99dd
Victor Pereira <vpereira@web.de>
2010-08-31 16:31:40 +0200

    added display feature

--- jflash.js
+++ jflash.js
@@ -1,27 +1,28 @@
 //Params:
 //message -> the message that should be passed
-//fadeIN -> ms for fadein effect
-//FadeOut -> ms for fadeout effect
-//timeout -> time that we will show our flash message
 //flash -> the flash that you want (notice, error)
 //author Victor Pereira <victor.pereira@bigrails.com>
 (function (jQuery) {
-	$.fn.jflash = function(params){
-	    var flash_div = jQuery(this);
-		params = $.extend( {message: "Action successfully executed", flash: "notice", fadein:600, fadout:600, timeout:1400}, params); 
+    var jflash_timer = 0;
+    $.fn.jflash = function(params){
+        var flash_div = jQuery(this);
+        params = $.extend( {
+            message: "Action successfully executed",
+            flash: "notice",
+            fadein:600,
+            display:1400,
+            fadeout:600
+        }, params);
 	    
-		var div_name = "flash_" + params.flash
-		flash_div.append("<div id="+ div_name + ">" + params.message  + "</div>");
-		flash_div.show();
-	    flash_div.fadeIn(params.fadein);
-	    // use Javascript timeout function to delay calling
-	
-	    setTimeout(function(){
-			flash_div.fadeOut(params.fadeout,function(){
-				flash_div.html("");
-	    		flash_div.hide()
-			})
-		},params.timeout);
-		return this;
-	};
+        var div_name = "flash_" + params.flash
+        flash_div.html("<div id="+ div_name + ">" + params.message  + "</div>");
+        flash_div.show();
+        flash_div.fadeIn(params.fadein);
+        // use Javascript timeout function to delay calling
+        clearTimeout(jflash_timer);
+        jflash_timer = setTimeout(function(){
+            flash_div.fadeOut(params.fadeout);
+        }, params.display);
+        return this;
+    };
 })(jQuery);
